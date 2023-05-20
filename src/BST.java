1422f4e454cd8ae17c9620522683f73dc806d52b
KarpovichAlexandr220174 <kukareku_147@mail.ru>
2023-05-20 19:12:15 +0600

    commit7

--- src/BST.java
+++ src/BST.java
@@ -1,9 +1,12 @@
+// importing main utils
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class BST<K extends Comparable<K>, V> {
     private Node root;
 
+    // Node class representing a node in the binary search tree
     private class Node {
         private K key;
         private V val;
@@ -13,14 +16,15 @@ public class BST<K extends Comparable<K>, V> {
             this.key = key;
             this.val = val;
         }
-
     }
 
+    // Inserts a key-value pair into the binary search tree
     public void put(K key, V value) {
         root = put(root, key, value);
     }
 
-    public Node put(Node node, K key, V val) {
+    // Recursive helper method to insert a key-value pair into the binary search tree
+    private Node put(Node node, K key, V val) {
         if (node == null) {
             return new Node(key, val);
         }
@@ -31,16 +35,18 @@ public class BST<K extends Comparable<K>, V> {
         } else if (cmp > 0) {
             node.right = put(node.right, key, val);
         } else {
-            node.val = val;
+            node.val = val; // Update value if key already exists
         }
         return node;
     }
 
+    // Retrieves the value associated with a given key in the binary search tree
     public V get(K key) {
         Node node = get(root, key);
         return node != null ? node.val : null;
     }
 
+    // Recursive helper method to search for a key in the binary search tree
     private Node get(Node node, K key) {
         if (node == null) {
             return null;
@@ -55,26 +61,29 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
-
+    // Deletes a key-value pair from the binary search tree
     public void delete(K key) {
         root = delete(root, key);
     }
 
-    public Node delete(Node node, K key) {
+    // Recursive helper method to delete a key-value pair from the binary search tree
+    private Node delete(Node node, K key) {
         if (node == null) {
             return null;
         }
         int cmp = key.compareTo(node.key);
-        if (cmp < 0)
+        if (cmp < 0) {
             node.left = delete(node.left, key);
-        else if (cmp > 0)
+        } else if (cmp > 0) {
             node.right = delete(node.right, key);
-        else {
+        } else {
+            // Node to delete found
             if (node.left == null) {
                 return node.right;
             } else if (node.right == null) {
                 return node.left;
             } else {
+                // Node to delete has both left and right child
                 Node minRightNode = findMin(node.right);
                 node.key = minRightNode.key;
                 node.val = minRightNode.val;
@@ -84,6 +93,7 @@ public class BST<K extends Comparable<K>, V> {
         return node;
     }
 
+    // Finds the minimum key in the binary search tree rooted at the given node
     public Node findMin(Node node) {
         while (node.left != null) {
             node = node.left;
@@ -91,24 +101,28 @@ public class BST<K extends Comparable<K>, V> {
         return node;
     }
 
+    // Returns an iterator for the binary search tree that iterates through the keys in ascending order
     public Iterable<K> iterator() {
         List<K> keys = new ArrayList<>();
         inorderTraversal(root, keys);
         return keys;
     }
+    // Performs an inorder traversal of the binary search tree and adds the keys to the list
 
     private void inorderTraversal(Node node, List<K> keys) {
         if (node == null)
             return;
-        inorderTraversal(node.left, keys);
-        keys.add(node.key);
-        inorderTraversal(node.right, keys);
+        inorderTraversal(node.left, keys); // Traverse left subtree
+        keys.add(node.key); // Add current node's key to the list
+        inorderTraversal(node.right, keys); // Traverse right subtree
     }
 
+    // Returns the size (number of nodes) of the binary search tree
     public int size() {
         return size(root);
     }
 
+    // Recursive helper method to calculate the size of the binary search tree
     private int size(Node node) {
         if (node == null) {
             return 0;
@@ -116,5 +130,4 @@ public class BST<K extends Comparable<K>, V> {
             return 1 + size(node.left) + size(node.right);
         }
     }
-
 }
