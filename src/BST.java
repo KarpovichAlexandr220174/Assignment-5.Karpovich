73790c582b2112adb8045465b4cc86ce8cd53350
KhusseinGashimov <rmit25@mail.ru>
2023-05-22 09:05:38 +0600

    HEIGHT COMMIT

--- src/BST.java
+++ src/BST.java
@@ -130,4 +130,18 @@ public class BST<K extends Comparable<K>, V> {
             return 1 + size(node.left) + size(node.right);
         }
     }
+
+    private int height() {
+        return height(root);
+    }
+
+    private int height(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
 }
